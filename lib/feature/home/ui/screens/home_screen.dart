f0b485152922a7deb2190d390774b30f662c74b8
osama gabr <125764981+oreWaOsama@users.noreply.github.com>
2025-06-16 19:24:25 +0300

    home screen ui

--- lib/feature/home/ui/screens/home_screen.dart
+++ lib/feature/home/ui/screens/home_screen.dart
@@ -1,10 +1,101 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:new_movie_app/core/helpers/app_image_assets.dart';
+import 'package:new_movie_app/core/theming/colors_manager.dart';
+import 'package:new_movie_app/core/theming/text_styles.dart';
+import 'package:new_movie_app/core/utils/locale_keys.dart';
+import 'package:new_movie_app/feature/home/ui/widgets/carousel_slider_body.dart';
+import 'package:new_movie_app/feature/home/ui/widgets/list_view_body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.65,
+          width: double.infinity,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Image.asset(
+                'assets/images/62a91993882ebf7b39030a68c87492f1e5e33643.jpg',
+                fit: BoxFit.cover,
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color.fromRGBO(0, 0, 0, 0.8),
+                      Color.fromRGBO(0, 0, 0, 0.6),
+                    ],
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      AssetsManager.availableNow,
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      height: MediaQuery.of(context).size.height * 0.1,
+                    ),
+                    const Spacer(),
+                    const CarouselSliderBody(),
+                    const Spacer(),
+                    Image.asset(
+                      AssetsManager.watchNow,
+                      width: MediaQuery.of(context).size.width * 0.75,
+                      height: MediaQuery.of(context).size.height * 0.1395,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            right: 16,
+            left: 16,
+          ),
+          child: Row(
+            children: [
+              Text(
+                LocaleKeys.action.tr(),
+                style: TextStyles.font18WhiteRegular,
+              ),
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: ColorsManager.black,
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      LocaleKeys.seeMore.tr(),
+                      style: TextStyles.font18WhiteRegular.copyWith(
+                        color: ColorsManager.gold,
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    const Icon(
+                      Icons.arrow_forward,
+                      color: ColorsManager.gold,
+                      size: 16,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        const ListViewBody(),
+      ],
+    );
   }
 }
