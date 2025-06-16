eea9521151eb59f2d36d26dcabe755cbe18ebbd1
osama gabr <125764981+oreWaOsama@users.noreply.github.com>
2025-06-16 19:14:43 +0300

    search screen ui

---
+++ lib/core/helpers/app_image_assets.dart
@@ -0,0 +1,12 @@
+class AssetsManager {
+  AssetsManager._();
+
+  static const String homeIcon = 'assets/svgs/home icon.svg';
+  static const String searchIcon = 'assets/svgs/search icon.svg';
+  static const String exploreIcon = 'assets/svgs/explore icon.svg';
+  static const String profileIcon = 'assets/svgs/profile icon.svg';
+  static const String watchNow = 'assets/images/Watch Now.png';
+  static const String availableNow = 'assets/images/Available Now.png';
+  static const String emptySearch =
+      'assets/images/svgs/Empty search screen.svg';
+}
