836588a60a6bacaf7bef835040fd683a2e8c8a1c
osama gabr <125764981+oreWaOsama@users.noreply.github.com>
2025-06-17 00:06:04 +0300

    ....

--- lib/movie_app.dart
+++ lib/movie_app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:new_movie_app/core/routes/app_routes.dart';
 import 'package:new_movie_app/core/theming/colors_manager.dart';
 
-import 'feature/home/ui/screens/user_home_view.dart';
 class MovieApp extends StatelessWidget {
   const MovieApp({super.key});
 
