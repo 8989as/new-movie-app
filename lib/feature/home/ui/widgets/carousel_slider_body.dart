7e6fe8a5ce001c9437d7261d2bfdbb7521cd97e0
mohamedabdeltwab7 <169074349+mohamedabdeltwab7@users.noreply.github.com>
2025-06-28 00:41:57 +0300

    home api done , movie details ui , explore ui and logic

--- lib/feature/home/ui/widgets/carousel_slider_body.dart
+++ lib/feature/home/ui/widgets/carousel_slider_body.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:new_movie_app/core/theming/colors_manager.dart';
 import 'package:new_movie_app/core/theming/text_styles.dart';
 
+import '../../../../core/routes/app_routes_name.dart';
+import '../../data/movie_serviece.dart';
+import '../../models/movie.dart';
+
 class CarouselSliderBody extends StatelessWidget {
   const CarouselSliderBody({
     super.key,
@@ -10,60 +14,74 @@ class CarouselSliderBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      options: CarouselOptions(
-        height: MediaQuery.of(context).size.height * 0.4,
-        enlargeCenterPage: true,
-        pageSnapping: true,
-        disableCenter: true,
-        viewportFraction: 0.6,
-      ),
-      items: [1, 2, 3, 4, 5].map((i) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Container(
-              width: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                image: const DecorationImage(
-                  image: AssetImage(
-                      'assets/images/62a91993882ebf7b39030a68c87492f1e5e33643.jpg'),
-                  fit: BoxFit.cover,
-                ),
+    return FutureBuilder<List<Movie>>(
+      future: MovieService.fetchMovies(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator(color: ColorsManager.gold,));
+        } else if (snapshot.hasError) {
+          return Center(child: Text("cannot load movie right now , try again later"));
+        } else {
+          final movies = snapshot.data!;
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(context , AppRoutesName.details),
+            child: CarouselSlider(
+              options: CarouselOptions(
+                height: MediaQuery.of(context).size.height * 0.4,
+                enlargeCenterPage: true,
+                pageSnapping: true,
+                disableCenter: true,
+                viewportFraction: 0.6,
               ),
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: 10,
-                    left: 10,
-                    child: Container(
-                      padding: const EdgeInsets.all(8.0),
+              items: movies.map((movie) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: 100,
                       decoration: BoxDecoration(
-                        color: ColorsManager.black.withOpacity(0.7),
-                        borderRadius: BorderRadius.circular(10),
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: NetworkImage(movie.image),
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                      child: Row(
+                      child: Stack(
                         children: [
-                          Text(
-                            '7.7',
-                            style: TextStyles.font16WhiteRegular,
-                          ),
-                          const SizedBox(width: 5),
-                          const Icon(
-                            Icons.star,
-                            color: ColorsManager.gold,
-                            size: 20,
+                          Positioned(
+                            top: 10,
+                            left: 10,
+                            child: Container(
+                              padding: const EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                color: ColorsManager.black.withOpacity(0.7),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    movie.rating.toString(),
+                                    style: TextStyles.font16WhiteRegular,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  const Icon(
+                                    Icons.star,
+                                    color: ColorsManager.gold,
+                                    size: 20,
+                                  ),
+                                ],
+                              ),
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                  )
-                ],
-              ),
-            );
-          },
-        );
-      }).toList(),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+          );
+        }
+      },
     );
   }
 }
